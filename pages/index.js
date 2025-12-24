9fece8cca670effaf1550e6fe6bebafc2f8550a7
Vinícius Amaral <amaralvinicius171@gmail.com>
2025-12-24 08:35:37 -0300

    Add initial page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+
+  return (
+    <h1 style={{
+      padding: "1rem",
+      margin: "0"
+    }}>
+      Hello, world! <a href="https://curso.dev" target="_blank">curso.dev</a>
+    </h1>
+  )
+}
+
+export default Home;
