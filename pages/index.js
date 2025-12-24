0c7f8ddeac1e722b3abfbc87533e6af5f4bedf12
Vinícius Amaral <amaralvinicius171@gmail.com>
2025-12-24 18:38:15 -0300

    Add `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 function Home() {
-
   return (
-    <h1 style={{
-      padding: "1rem",
-      margin: "0",
-      display: "flex",
-      flexDirection: "column"
-    }}>
+    <h1
+      style={{
+        padding: "1rem",
+        margin: "0",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       Hello, world!
-      <a href="https://curso.dev" target="_blank">curso.dev</a>
+      <a href="https://curso.dev" target="_blank">
+        curso.dev
+      </a>
     </h1>
-  )
+  );
 }
 
 export default Home;
